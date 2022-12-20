50c8a0d01bb6f3b316ee807a272b4e9db322b588
SalahAdDin <alagunasalahaddin@live.com>
2022-12-21 00:49:02 +0300

    build: :wrench: Updating the commitlint setup to support the new format.

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,165 +1,30 @@
-import {
-  Commit,
-  LintOptions,
-  Plugin,
-  QualifiedRules,
-  Rule,
-  UserConfig,
-} from '@commitlint/types';
+import { LintOptions, QualifiedRules, UserConfig } from "@commitlint/types";
 
-declare module '@commitlint/types' {
+declare module "@commitlint/types" {
   interface Commit {
     ticket: string | null;
   }
 }
 
 const types = [
-  ':adhesive_bandage:',
-  ':alembic:',
-  ':alien:',
-  ':ambulance:',
-  ':animation:',
-  ':arrow_down:',
-  ':arrow_up:',
-  ':art:',
-  ':beers:',
-  ':bento:',
-  ':bookmark:',
-  ':boom:',
-  ':bug:',
-  ':building_construction:',
-  ':bulb:',
-  ':busts_in_silhouette:',
-  ':camera_flash:',
-  ':card_file_box:',
-  ':chart_with_upwards_trend:',
-  ':children_crossing:',
-  ':clown_face:',
-  ':construction_worker:',
-  ':construction:',
-  ':egg:',
-  ':fire:',
-  ':globe_with_meridians:',
-  ':goal_net:',
-  ':green_heart:',
-  ':hammer:',
-  ':coffin:',
-  ':heavy_minus_sign:',
-  ':heavy_plus_sign:',
-  ':iphone:',
-  ':label:',
-  'lipstick:',
-  ':lock:',
-  ':closed_lock_with_key:',
-  ':loud_sound:',
-  ':mag:',
-  ':memo:',
-  ':mute:',
-  ':ok_hand:',
-  ':package:',
-  ':page_facing_up:',
-  ':passport_control:',
-  ':pencil:',
-  ':pencil2:',
-  ':poop:',
-  ':pushpin:',
-  ':recycle:',
-  ':rewind:',
-  ':rocket:',
-  ':rotating_light:',
-  ':see_no_evil:',
-  ':seedling:',
-  ':sparkles:',
-  ':speech_balloon:',
-  ':stethoscope:',
-  ':tada:',
-  ':test_tube:',
-  ':triangular_flag_on_post:',
-  ':truck:',
-  ':twisted_rightwards_arrows:',
-  ':wastebasket:',
-  ':wheelchair:',
-  ':white_check_mark:',
-  ':wrench:',
-  ':zap:',
-  ':monocle_face:',
-  ':necktie:',
-  ':bricks:',
-  ':technologist:',
-  ':money_with_wings:',
+  "build",
+  "chore",
+  "ci",
+  "docs",
+  "feat",
+  "fix",
+  "perf",
+  "refactor",
+  "revert",
+  "style",
+  "test",
 ];
-
-// @deprecated Regex based on https://regex101.com/r/L5f7hN/1
-// Regex based on https://regex101.com/r/DFWyx5/1
-
-/**
- * If we want to make the ticket required, remove `?` after the last `)`.
- */
-const planableTicketWithSpaceAfter = /^(?:\[([A-z]*-\d+)\]\s)?/;
-
-const anyStringEmoji = /(:\w*:)/;
-
-const composedOptionalScope = /(?:\([^()\r\n]*\)|\()?(!)?/;
-
-/**
- * If we want to force a `:` before the subject, add `:` after `<subject>`.
- */
-const optionalSubject = /(.*)?/;
-
-/**
- * To make `ticket` required, get it from `parsed` and check it to not be null
- * @param parsed
- * @returns [boolean]
- */
-const headerMatchPlanablePattern: Rule = (parsed: Commit) => {
-  const { type, subject } = parsed;
-
-  if (type === null && subject === null) {
-    return [
-      false,
-      "header must be in format '[P-11] :tada: Replace footer' or ':recycle: Replace footer'",
-    ];
-  }
-  return [true];
-};
-
-const ticketPattern: Rule = (parsed: Commit) => {
-  // Based on https://regex101.com/r/IMkdYo/1
-  const planableTicket = /^(\[([A-z]*-\d+)\])/;
-
-  const { ticket } = parsed;
-
-  if (ticket) {
-    if (!planableTicket.test(`[${ticket}]`)) {
-      return [
-        false,
-        'ticket must be in format of a valid Linear issue, for example: [P-11]',
-      ];
-    }
-  }
-  return [true];
-};
-
-const planablePlugin: Plugin = {
-  rules: {
-    'header-match-planable-pattern': headerMatchPlanablePattern,
-    'ticket-match-pattern': ticketPattern,
-  },
-};
 
 const parserPreset: LintOptions = {
   parserOpts: {
-    headerPattern: new RegExp(
-      planableTicketWithSpaceAfter.source +
-        anyStringEmoji.source +
-        composedOptionalScope.source +
-        optionalSubject.source,
-      'u',
-    ),
-    headerCorrespondence: ['ticket', 'type', 'scope', 'breaking', 'subject'],
-  },
-  plugins: {
-    planable: planablePlugin,
+    // https://regex101.com/r/7znOoR/1
+    headerPattern: /^(?:\[([A-z]*-\d+)]\s)?(\w*)(?:\((.*)\))?!?:\s(.*)$/,
+    headerCorrespondence: ["ticket", "type", "scope", "subject"],
   },
 };
 
@@ -171,17 +36,16 @@ const rules: QualifiedRules = {
   // 'footer-empty': [2, 'always'],
   // 'references-empty': [2, 'never'],
 
-  'header-match-planable-pattern': [2, 'always'],
-  'ticket-match-pattern': [2, 'always'],
-  'type-enum': [2, 'always', types],
-  'subject-case': [2, 'always', 'sentence-case'],
+  "subject-full-stop": [2, "always", "."],
+  "scope-case": [2, "always", "lower-case"],
+  "type-enum": [2, "always", types],
 };
 
 const Configuration: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
   helpUrl:
-    'https://github.com/SalahAdDin/react-ts-vite-template#emojitype-descriptions',
+    "https://github.com/SalahAdDin/react-ts-vite-template#commit-convention",
   parserPreset,
-  plugins: [planablePlugin],
   rules,
 };
 
