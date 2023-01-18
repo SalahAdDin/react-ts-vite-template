f35d4162da77807d2461420f04dfc2eac4a105af
SalahAdDin <alagunasalahaddin@live.com>
2023-01-19 02:19:37 +0300

    feat: :wrench: Loading url related values from env file.
    In this way we ensure Playwright will open the project url settled by vite.

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,51 @@
 import path from "path";
+import { loadEnv } from "vite";
+import type { ConfigEnv } from "vite";
 import { defineConfig } from "vitest/config";
 
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    devSourcemap: true,
-  },
-  build: { sourcemap: true },
-  resolve: {
-    alias: {
-      "@application": path.resolve(__dirname, "src/application"),
-      "@domain": path.resolve(__dirname, "src/domain"),
-      "@infrastructure": path.resolve(__dirname, "src/infrastructure"),
-      "@presentation": path.resolve(__dirname, "src/presentation"),
+export default (configEnv: ConfigEnv) => {
+  process.env = {
+    ...process.env,
+    ...loadEnv(configEnv.mode, process.cwd(), "VITE_"),
+  };
+
+  return defineConfig({
+    base: process.env.VITE_APP_BASE_URL,
+    plugins: [react()],
+    css: {
+      devSourcemap: true,
     },
-  },
-  test: {
-    globals: true,
-    watch: false,
-    coverage: {
-      reporter: ["text", "html"],
-      branches: 80,
-      functions: 80,
-      // include: ["/src"],
-      lines: 80,
-      statements: -10,
+    build: { sourcemap: true },
+    resolve: {
+      alias: {
+        "@application": path.resolve(__dirname, "src/application"),
+        "@domain": path.resolve(__dirname, "src/domain"),
+        "@infrastructure": path.resolve(__dirname, "src/infrastructure"),
+        "@presentation": path.resolve(__dirname, "src/presentation"),
+      },
     },
-    passWithNoTests: true,
-    environment: "happy-dom",
-    setupFiles: "./src/vitest.setup.ts",
-  },
-  server: {
-    open: true,
-  },
-});
+    test: {
+      globals: true,
+      watch: false,
+      coverage: {
+        reporter: ["text", "html"],
+        branches: 80,
+        functions: 80,
+        // include: ["/src"],
+        lines: 80,
+        statements: -10,
+      },
+      passWithNoTests: true,
+      environment: "happy-dom",
+      setupFiles: "./src/vitest.setup.ts",
+    },
+    server: {
+      open: true,
+      host: process.env.VITE_SERVER_HOST,
+      port: Number(process.env.VITE_SERVER_PORT),
+    },
+  });
+};
