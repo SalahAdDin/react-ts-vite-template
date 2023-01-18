7c29bf559def1aeb9a67b733eef43be90e711e41
SalahAdDin <alagunasalahaddin@live.com>
2023-01-19 01:12:45 +0300

    chore: :wastebasket: Commenting and cleaning code as documentation.

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,37 +1,20 @@
 import { rest } from "msw";
-import { nanoid } from "nanoid";
 
-import { IBaseUser, IUser } from "@domain/user.dto";
+// import users from "./entities/users.json" assert { type: "json" };
+// import shuffleArray from "./utils";
 
-import users from "./entities/users.json" assert { type: "json" };
-import shuffleArray from "./utils";
+const handlers = [
+  rest.get("/users", () => {}),
 
-let raw = shuffleArray<IUser>(users);
-
-export const handlers = [
-  rest.get<IUser[]>("/users", (req, res, ctx) =>
-    res(ctx.status(200), ctx.json(raw)),
-  ),
-
-  rest.get<IUser>("/user/:id", (req, res, ctx) => {
-    const { id } = req.params;
-
-    if (id) {
-      const userId = raw.findIndex((item) => item.id === id);
-      if (userId !== -1) {
-        return res(ctx.status(200), ctx.json(raw[userId]));
-      }
-      return res(ctx.status(404));
-    }
-    return res(ctx.status(400));
-  }),
+  /*
+  rest.get("/user/:id", (req, res, ctx) => {}),
 
   rest.post<IBaseUser>("/user/create", async (req, res, ctx) => {
     const body = await req.json();
 
     if (body?.firstName) {
       const user: IUser = {
-        id: nanoid(),
+        id: crypto.randomUUID(),
         ...body,
       };
 
@@ -67,7 +50,7 @@ export const handlers = [
     }
 
     return res(ctx.status(400));
-  }),
+  }), */
 ];
 
 export default handlers;
