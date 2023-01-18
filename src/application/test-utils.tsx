3a01497c06fe13c8293b0703ae5f0e5afef7e9d5
SalahAdDin <alagunasalahaddin@live.com>
2023-01-19 01:17:39 +0300

    chore: :toolbox: Adding some utils for testing.

---
+++ src/application/test-utils.tsx
@@ -0,0 +1,78 @@
+// import { BrowserRouter } from "react-router-dom";
+import { afterEach } from "vitest";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { cleanup, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+afterEach(() => {
+  cleanup();
+});
+
+const customRender = (ui: React.ReactElement, options = {}) =>
+  render(ui, {
+    wrapper: ({ children }) => children,
+    ...options,
+  });
+
+const createTestQueryClient = () =>
+  new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+    logger: {
+      log: console.log,
+      warn: console.warn,
+      error: () => {},
+    },
+  });
+
+function renderWithClient(ui: React.ReactElement) {
+  const testQueryClient = createTestQueryClient();
+  const { rerender, ...result } = render(
+    <QueryClientProvider client={testQueryClient}>{ui}</QueryClientProvider>
+  );
+
+  return {
+    ...result,
+    rerender: (rerenderUi: React.ReactElement) =>
+      rerender(
+        <QueryClientProvider client={testQueryClient}>
+          {rerenderUi}
+        </QueryClientProvider>
+      ),
+  };
+}
+
+function createWrapper() {
+  const testQueryClient = createTestQueryClient();
+
+  const wrapper = ({ children }: { children: React.ReactNode }) => (
+    <QueryClientProvider client={testQueryClient}>
+      {children}
+    </QueryClientProvider>
+  );
+
+  return wrapper;
+}
+
+const renderWithRouter = (ui: React.ReactElement, { route = "/" } = {}) => {
+  window.history.pushState({}, "Test page", route);
+
+  return {
+    user: userEvent.setup(),
+    //    ...render(ui, { wrapper: BrowserRouter }),
+  };
+};
+
+export * from "@testing-library/dom";
+export { default as userEvent } from "@testing-library/user-event";
+
+export {
+  customRender as render,
+  renderWithClient,
+  renderWithRouter,
+  createWrapper,
+};
