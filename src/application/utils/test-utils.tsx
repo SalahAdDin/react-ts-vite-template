37b048b8e64cc4ef4a59147ddf551d246c417029
Luis Alaguna <alagunasalahaddin@live.com>
2024-08-09 11:51:44 -0500

    chore: :wrench: cleaning test utils

--- src/application/utils/test-utils.tsx
+++ src/application/utils/test-utils.tsx
@@ -1,29 +1,21 @@
-// import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-const customRender = (ui: React.ReactElement, options = {}) =>
-  render(ui, {
+const customRender = (ui: React.ReactElement, options = {}) => render(ui, {
     wrapper: ({ children }) => children,
     ...options,
   });
 
-const createTestQueryClient = () =>
-  new QueryClient({
+const createTestQueryClient = () => new QueryClient({
     defaultOptions: {
       queries: {
         retry: false,
       },
     },
-    logger: {
-      log: console.log,
-      warn: console.warn,
-      error: () => {},
-    },
   });
 
-function renderWithClient(ui: React.ReactElement) {
+const renderWithClient = (ui: React.ReactElement) => {
   const testQueryClient = createTestQueryClient();
   const { rerender, ...result } = render(
     <QueryClientProvider client={testQueryClient}>{ui}</QueryClientProvider>
@@ -31,42 +23,23 @@ function renderWithClient(ui: React.ReactElement) {
 
   return {
     ...result,
-    rerender: (rerenderUi: React.ReactElement) =>
-      rerender(
+    rerender: (rerenderUi: React.ReactElement) => rerender(
         <QueryClientProvider client={testQueryClient}>
           {rerenderUi}
         </QueryClientProvider>
       ),
   };
-}
-
-function createWrapper() {
-  const testQueryClient = createTestQueryClient();
-
-  const wrapper = ({ children }: { children: React.ReactNode }) => (
-    <QueryClientProvider client={testQueryClient}>
-      {children}
-    </QueryClientProvider>
-  );
-
-  return wrapper;
-}
+};
 
 const renderWithRouter = (ui: React.ReactElement, { route = "/" } = {}) => {
   window.history.pushState({}, "Test page", route);
 
   return {
     user: userEvent.setup(),
-    //    ...render(ui, { wrapper: BrowserRouter }),
   };
 };
 
 export * from "@testing-library/dom";
 export { default as userEvent } from "@testing-library/user-event";
 
-export {
-  customRender as render,
-  renderWithClient,
-  renderWithRouter,
-  createWrapper,
-};
+export { customRender as render, renderWithClient, renderWithRouter };
